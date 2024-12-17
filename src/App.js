72d17a07ce1d72e45170a4d738b91b2dab412f62
Kratos1950 <87760385+Katos1950@users.noreply.github.com>
2024-12-16 22:45:28 -0600

    added a basic header
    install react router dom and tailwind css for the header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import { Grid } from './Grid';
 import { GridProvider } from './GridContext';
+import {Header} from './Header';
+import { Routes,Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <GridProvider>
-        <Grid/>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Grid/>}></Route>
+          </Routes>
       </GridProvider>
     </div>
   );
