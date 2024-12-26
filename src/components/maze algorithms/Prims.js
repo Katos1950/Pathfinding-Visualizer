60af4e6607f2795bb3571ed071ec65d271f8574c
Kratos1950 <87760385+Katos1950@users.noreply.github.com>
2024-12-25 19:54:38 -0600

    visualize prims maze

--- src/components/maze algorithms/Prims.js
+++ src/components/maze algorithms/Prims.js
@@ -1,6 +1,7 @@
 export const Prims = (grid,rows,cols,setGrid,setShortestPath) => {
     const startNode = grid.flat().find(node => node.isStartNode);
     const endNode = grid.flat().find(node => node.isEndNode);
+    const mazePath = [];
 
     const assignEdges = (node) => {
 
@@ -37,61 +38,67 @@ export const Prims = (grid,rows,cols,setGrid,setShortestPath) => {
             endNode.isWall = false;
             let frontierCells = {}; 
             let [currRow, currCol] = [startNode.rowIndex,startNode.colIndex];
-            do{
-            Object.entries(grid[currRow][currCol].edges).forEach(([key, value]) => {
-                const [row, col] = key.split(" ").map(Number);
-                if(currRow < row){
-                    Object.entries(grid[row][col].edges).forEach(([keyf, valuef]) => {
-                        const [row2, col2] = keyf.split(" ").map(Number);
-                        if(row < row2 && grid[row2][col2].isWall)
-                            frontierCells[keyf] = key;
-                    })
-                } 
 
-                else if (currRow > row){
-                    Object.entries(grid[row][col].edges).forEach(([keyf, valuef]) => {
-                        const [row2, col2] = keyf.split(" ").map(Number);
-                        if(row > row2 && grid[row2][col2].isWall)
-                            frontierCells[keyf] = key;
-                    })
+            const intervalId = setInterval(() => {
+                Object.entries(grid[currRow][currCol].edges).forEach(([key, value]) => {
+                    const [row, col] = key.split(" ").map(Number);
+                    if (currRow < row) {
+                        Object.entries(grid[row][col].edges).forEach(([keyf, valuef]) => {
+                            const [row2, col2] = keyf.split(" ").map(Number);
+                            if (row < row2 && grid[row2][col2].isWall)
+                                frontierCells[keyf] = key;
+                        });
+                    } else if (currRow > row) {
+                        Object.entries(grid[row][col].edges).forEach(([keyf, valuef]) => {
+                            const [row2, col2] = keyf.split(" ").map(Number);
+                            if (row > row2 && grid[row2][col2].isWall)
+                                frontierCells[keyf] = key;
+                        });
+                    } else if (currCol < col) {
+                        Object.entries(grid[row][col].edges).forEach(([keyf, valuef]) => {
+                            const [row2, col2] = keyf.split(" ").map(Number);
+                            if (col < col2 && grid[row2][col2].isWall)
+                                frontierCells[keyf] = key;
+                        });
+                    } else if (currCol > col) {
+                        Object.entries(grid[row][col].edges).forEach(([keyf, valuef]) => {
+                            const [row2, col2] = keyf.split(" ").map(Number);
+                            if (col > col2 && grid[row2][col2].isWall)
+                                frontierCells[keyf] = key;
+                        });
+                    }
+                });
+            
+                const arrayOfEntries = Object.entries(frontierCells);
+                console.log("Frontier cell ");
+                console.log(arrayOfEntries);
+            
+                if (arrayOfEntries.length === 0) {
+                    clearInterval(intervalId); // Stop the interval when no frontier cells are left
+                    console.log("All frontier cells processed.");
+                    return;
                 }
-
-                else if(currCol < col){
-                    Object.entries(grid[row][col].edges).forEach(([keyf, valuef]) => {
-                        const [row2, col2] = keyf.split(" ").map(Number);
-                        if(col < col2 && grid[row2][col2].isWall)
-                            frontierCells[keyf] = key;
-                    })
-                }
-                else if(currCol > col){
-                    Object.entries(grid[row][col].edges).forEach(([keyf, valuef]) => {
-                        const [row2, col2] = keyf.split(" ").map(Number);
-                        if(col > col2 && grid[row2][col2].isWall)
-                            frontierCells[keyf] = key;
-                    })
-                }
-            })
-
-            const arrayOfEntries = Object.entries(frontierCells)
-            console.log("Frontier cell ")
-            console.log(arrayOfEntries);
-            const randomElementIndex = Math.floor(Math.random() * arrayOfEntries.length);
-            const randomElemenent = arrayOfEntries[randomElementIndex]
-            console.log("random element to remove " + randomElemenent);
-            const frontierCellKey = randomElemenent[0]
-            const [frontierCellRow, frontierCellCol] = randomElemenent[0].split(" ")
-            const [connectingRow, connectingCol] = randomElemenent[1].split(" ")
-
-            grid[frontierCellRow][frontierCellCol].isWall = false;
-            grid[connectingRow][connectingCol].isWall = false;
-
-            currRow = frontierCellRow;
-            currCol = frontierCellCol;
-
-            delete frontierCells[frontierCellKey];
-            console.log("After deleting")
-            console.log(Object.entries(frontierCells))
-            }while(Object.entries(frontierCells).length > 0);
+            
+                const randomElementIndex = Math.floor(Math.random() * arrayOfEntries.length);
+                const randomElemenent = arrayOfEntries[randomElementIndex];
+                console.log("random element to remove " + randomElemenent);
+            
+                const frontierCellKey = randomElemenent[0];
+                const [frontierCellRow, frontierCellCol] = frontierCellKey.split(" ").map(Number);
+                const [connectingRow, connectingCol] = randomElemenent[1].split(" ").map(Number);
+            
+                grid[frontierCellRow][frontierCellCol].isWall = false;
+                grid[connectingRow][connectingCol].isWall = false;
+            
+                currRow = frontierCellRow;
+                currCol = frontierCellCol;
+            
+                delete frontierCells[frontierCellKey];
+                console.log("After deleting");
+                console.log(Object.entries(frontierCells));
+                setGrid([...grid])
+            }, 20);
+            
             
 
         return (
